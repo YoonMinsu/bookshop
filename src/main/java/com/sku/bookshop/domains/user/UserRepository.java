a8c29bd8063b0b2e2390702f1d9bd81301716a6b
yoonminsu <yoonminsu97@gmail.com>
2020-11-09 19:17:43 +0900

    회원 레파지토리 작성

---
+++ src/main/java/com/sku/bookshop/domains/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sku.bookshop.domains.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
