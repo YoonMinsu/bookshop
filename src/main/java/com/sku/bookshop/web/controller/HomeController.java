d39edebf90622537219f984bb10463afa0ad35cb
yoonminsu <yoonminsu97@gmail.com>
2020-11-09 23:16:06 +0900

    HomeController 추가 ("/main", "/login", "/join")

---
+++ src/main/java/com/sku/bookshop/web/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.sku.bookshop.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+}
