88f61fe191f4183004e628c979b247e3a653af98
yoonminsu <yoonminsu97@gmail.com>
2020-11-11 14:52:29 +0900

    회원 컨트롤러 수정

--- src/main/java/com/sku/bookshop/web/controller/AccountController.java
+++ src/main/java/com/sku/bookshop/web/controller/AccountController.java
@@ -1,9 +1,14 @@
 package com.sku.bookshop.web.controller;
 
 import com.sku.bookshop.service.MemberService;
+import com.sku.bookshop.web.dtos.MemberRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RequiredArgsConstructor
@@ -23,7 +28,14 @@ public class AccountController {
     }
 
     @GetMapping("/join")
-    public String joinForm() {
+    public String joinForm(Model model) {
+        model.addAttribute(new MemberRequestDto());
         return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinForm(MemberRequestDto requestDto) {
+        memberService.saveMember(requestDto);
+        return "redirect:/login";
     }
 }
