edf0d23e1e1b743f7405f85153e232e5f1de1f7d
yoonminsu <yoonminsu97@gmail.com>
2020-11-11 21:57:40 +0900

    Member 엔티티 수정 [ 코드 리팩토링 必 ]

--- src/main/java/com/sku/bookshop/domain/member/Member.java
+++ src/main/java/com/sku/bookshop/domain/member/Member.java
@@ -6,13 +6,19 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Member extends BaseEntity {
+public class Member extends BaseEntity implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_num")
@@ -54,4 +60,33 @@ public class Member extends BaseEntity {
         this.role = role;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(Role.MEMBER.getKey()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
