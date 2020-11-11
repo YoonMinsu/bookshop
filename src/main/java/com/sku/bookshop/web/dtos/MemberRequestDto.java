33ea8c5fefdc75442ec01fafb71bb67acb6fc1d7
yoonminsu <yoonminsu97@gmail.com>
2020-11-11 20:52:06 +0900

    회원 회원가입 요청 DTO 수정

--- src/main/java/com/sku/bookshop/web/dtos/MemberRequestDto.java
+++ src/main/java/com/sku/bookshop/web/dtos/MemberRequestDto.java
@@ -17,38 +17,22 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class MemberRequestDto {
 
-    @NotBlank(message = "필수 항목입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,19}$",
-            message = "특수문자를 제외한 3~19자리 이내로 입력해주세요"
-    )
+    @NotBlank
     private String userid;
 
-    @NotBlank(message = "필수 항목입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 숫자와 영어 그리고 특수문자를 포함한 8~20자리 이내로 입력해주세요"
-    )
+    @NotBlank
     private String password;
 
-    @NotBlank(message = "필수 항목입니다")
-    @Pattern(regexp = "^[가-힣]*$",
-            message = "한글만 작성가능합니다."
-
-    )
+    @NotBlank
     private String name;
 
-    @Email(message = "이메일 형식을 맟춰주세요")
-    @NotBlank(message = "필수 항목입니다.")
+    @NotBlank
     private String email;
 
-    @NotBlank(message = "필수 항목입니다.")
-    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$",
-            message = "전화번호 형식에 맟춰주세요"
-    )
+    @NotBlank
     private String phoneNumber;
 
-    @NotBlank(message = "필수 항목입니다.")
-    @Pattern(regexp = "^[0-9]{8,8}$",
-    message = "형식에 형식에 맟춰주세요")
+    @NotBlank
     private String birth;
 
     private String zipcode;
