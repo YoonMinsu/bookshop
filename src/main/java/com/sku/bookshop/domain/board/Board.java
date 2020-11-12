16a82615e50e3bead0c62f5b98687c0be20bcfc1
yoonminsu <yoonminsu97@gmail.com>
2020-11-12 22:24:57 +0900

    Board 엔티티 작성

---
+++ src/main/java/com/sku/bookshop/domain/board/Board.java
@@ -0,0 +1,36 @@
+package com.sku.bookshop.domain.board;
+
+import com.sku.bookshop.common.BaseEntity;
+import com.sku.bookshop.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Board extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_num")
+    private Long num;
+
+    private String title;
+
+    private String writer;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_num")
+    private Member member;
+
+    @Builder
+    public Board(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+}
